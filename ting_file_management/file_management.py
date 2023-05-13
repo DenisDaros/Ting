ecc55afdd1c2f8573754acceb1b3298a17be7c59
denis daros <denisv.daros@gmail.com>
2023-05-13 15:57:50 -0300

    requisito 2 Implementando função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            raise ValueError("Formato inválido")
+
+        with open(path_file) as file:
+            return file.read().split("\n") 
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    except ValueError as e:
+        print(str(e), file=sys.stderr)
